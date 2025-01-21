a275ca119616cae1cc5d7986b6509501d28318d6
Ramzan <priceramzan9@gmail.com>
2025-01-22 04:06:03 +0500

    editclient.ts

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -3,9 +3,10 @@ import { createClient } from 'next-sanity'
 import { apiVersion, dataset, projectId } from '../env'
 
 export const client = createClient({
-  projectId,
-  dataset,
-  apiVersion,
-  useCdn:true,
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+  apiVersion:"2023-01-01",
+
+  useCdn:false,
   // Set to false if statically generating pages, using ISR or tag-based revalidation
 })
